660e1a4aae98793ef6d7df868c250c285f446a0c
lijo-cloud <lijo@researck.com>
2026-05-20 13:04:41 +0530

    edited hero3

--- app/api/health/route.js
+++ app/api/health/route.js
@@ -1,6 +1,6 @@
 export async function GET() {
   return Response.json(
-  { status: "failed",
+  { status: "ok",
     hostname: process.env.HOSTNAME || "unknown"
-  }, { status: 500 });
+  }, { status: 200 });
 }

91f63fa96d438e29021e5ab69284ebc21b68f879
lijo-cloud <lijo@researck.com>
2026-06-12 12:59:18 +0530

    modified health controller1

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -18,7 +18,7 @@ export class HealthController {
   async check() {
     try {
       return await this.health.check([
-        () => this.http.pingCheck('frontend-database', 'http://app-web-1:3000/api/db-test'),
+        () => this.http.pingCheck('frontend-database', 'http://api:3000/api/db-test'),
       ]);
     } catch (error) {
       // FALLBACK: If Next.js isn't up yet during boot, return a temporary safe status 
